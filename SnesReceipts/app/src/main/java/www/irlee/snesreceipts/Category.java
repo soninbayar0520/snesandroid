88e8356c6d7db8982ce373884fb72089d3ea2976
Soninbayar Munkhbayar <msoninbayar@liveperson.com>
2019-11-19 16:36:58 -0500

    model fixed

--- SnesReceipts/app/src/main/java/www/irlee/snesreceipts/Category.java
+++ SnesReceipts/app/src/main/java/www/irlee/snesreceipts/Category.java
@@ -1,4 +1,73 @@
 package www.irlee.snesreceipts;
 
 public class Category {
+
+    String categoryId;
+    String categoryName;
+    String categoryPicture;
+    String categoryDetails;
+    String createdOn;
+    String updatedOn;
+
+    public Category(String categoryId, String categoryName, String categoryPicture, String categoryDetails, String createdOn, String updatedOn) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.categoryPicture = categoryPicture;
+        this.categoryDetails = categoryDetails;
+        this.createdOn = createdOn;
+        this.updatedOn = updatedOn;
+    }
+
+    public Category() {
+    }
+
+    public String getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(String updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getCategoryPicture() {
+        return categoryPicture;
+    }
+
+    public void setCategoryPicture(String categoryPicture) {
+        this.categoryPicture = categoryPicture;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryDetails() {
+        return categoryDetails;
+    }
+
+    public void setCategoryDetails(String categoryDetails) {
+        this.categoryDetails = categoryDetails;
+    }
 }
--- SnesReceipts/app/src/main/java/www/irlee/snesreceipts/MainActivity.java
+++ SnesReceipts/app/src/main/java/www/irlee/snesreceipts/MainActivity.java
@@ -8,20 +8,24 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,20 +39,51 @@ public class MainActivity extends AppCompatActivity {
         editText.setText(message);
     }
 
+    public void getdata(String models) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Category> categories = null;
+            categories = Arrays.asList(mapper.readValue(models, Category[].class));
+            for (Category category : categories) {
+                System.out.println(category.categoryName);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-    public void GetAllCategories(View view)   {
+    }
+
+
+    public void GetAllCategories(View view) {
 
         try {
 
             RequestQueue queue = Volley.newRequestQueue(this);
-            
+            String url = "http://snes.irlee.net/snes/GetAllCategories";
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            // Display the first 500 characters of the response string.
+
+                            TextView tView = findViewById(R.id.txt_View);
+                            tView.setText("done");
+                            getdata(response);
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            queue.add(stringRequest);
 
 
-        }catch (Exception e){
-            Log.i("Error",e.getMessage());
-        }
-        finally {
+        } catch (Exception e) {
+            Log.i("Error", e.getMessage());
+        } finally {
 
         }
     }
